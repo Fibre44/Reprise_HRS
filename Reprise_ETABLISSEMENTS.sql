1469111dc42987de00959d807b835f5878bb200f
Nicolas Denis <NDENIS@cegid.com>
2020-07-29 17:12:34 +0200

    Ventilation du script en plusieurs scripts par tables

---
+++ Reprise_ETABLISSEMENTS.sql
@@ -0,0 +1,179 @@
+USE REPRISE_HRU
+
+
+--Gestion des nouveaux codes établissements
+--attention dans la table Etablissement on trouve plusieurs fois les mêmes valeurs
+
+DECLARE
+
+@VAR_SOCIETE_LIGNE int,--société en cours
+@VAR_SOCIETE_TOTAL int,--total des sociétés
+@VAR_SOCIETE_CEMP char(4),--société à traiter
+@VAR_ETABLISSEMENT_LIGNE int, --établissement en cours
+@VAR_ETABLISSEMENT_TOTAL int, --total des établissement
+@VAR_SOCIETE_ETABLISSEMENT char(4), --va lire les 4 première position de gauche du champ etablissement dans etablissement pour trouver la société
+@VAR_ETABLISSEMENT_CODE_HRU varchar(35),--code établissement HRU
+@VAR_ETABLISSEMENT_CODE_HRS_TEMP int, --Incrémentation des établissements
+@VAR_ETABLISSEMENT_CODE_HRS char(3),--code établissement HRS
+@VAR_RAISON_SOCIALE varchar(255),
+@VAR_NATURECONTROLE varchar(255),
+@VAR_RESULTATCONTROLE varchar(255),
+@VAR_COMMENTAIRE varchar(255),
+@VAR_TYPE_TRANSCO varchar(35),--alimente la table de transco
+@VAR_TYPE_VALEUR_HRU int,--test si l'établissement existe déjà dans la table de transco
+@VAR_TRA_SIREN varchar(35); --SIREN de la société
+
+SET @VAR_TYPE_TRANSCO='Etablissement'
+SET @VAR_ETABLISSEMENT_CODE_HRS_TEMP=0;
+SET @VAR_SOCIETE_LIGNE=1; 
+SET @VAR_ETABLISSEMENT_LIGNE=1;
+
+SELECT @VAR_SOCIETE_TOTAL=COUNT(*) FROM EMPLOYEUR;
+SELECT @VAR_ETABLISSEMENT_TOTAL=COUNT(*) FROM ETABLISSEMENT;
+
+WHILE @VAR_SOCIETE_LIGNE<=@VAR_SOCIETE_TOTAL --boucle par société
+
+BEGIN
+
+SELECT @VAR_SOCIETE_CEMP=Employeur FROM EMPLOYEUR WHERE @VAR_SOCIETE_LIGNE=SOCIETE_LIGNE;--récupération de la société à traiter
+SELECT @VAR_TRA_SIREN=Siren FROM EMPLOYEUR WHERE @VAR_SOCIETE_LIGNE=SOCIETE_LIGNE; --récupération du SIREN
+
+
+	WHILE @VAR_ETABLISSEMENT_LIGNE<=@VAR_ETABLISSEMENT_TOTAL --boucle par établissement
+
+	BEGIN
+
+	SELECT @VAR_SOCIETE_ETABLISSEMENT=LEFT(Etablissement,4) FROM ETABLISSEMENT WHERE  ETABLISSEMENT_LIGNE=@VAR_ETABLISSEMENT_LIGNE;--récupération du code société
+		
+		IF @VAR_SOCIETE_ETABLISSEMENT=@VAR_SOCIETE_CEMP --Si le code société de l'établissement correspond à l'établissement en cours alors traitement
+
+			BEGIN
+
+			SELECT @VAR_ETABLISSEMENT_CODE_HRU=Etablissement FROM ETABLISSEMENT WHERE ETABLISSEMENT_LIGNE=@VAR_ETABLISSEMENT_LIGNE;
+			SELECT @VAR_TYPE_VALEUR_HRU=COUNT(*) FROM TRANSCO WHERE TRA_VALEURHRU=@VAR_ETABLISSEMENT_CODE_HRU;--recherche si l'établissement a déjà été traité
+
+			IF @VAR_TYPE_VALEUR_HRU=0 --Test si l'établissement a déjà été importé
+
+				BEGIN
+
+				SET @VAR_ETABLISSEMENT_CODE_HRS_TEMP=@VAR_ETABLISSEMENT_CODE_HRS_TEMP+1;--Incrémentation du compteur
+			
+				
+					IF @VAR_ETABLISSEMENT_CODE_HRS_TEMP<10--Ajout des 0 
+
+						BEGIN
+				
+						SET @VAR_ETABLISSEMENT_CODE_HRS=CONCAT('00',CAST(@VAR_ETABLISSEMENT_CODE_HRS_TEMP AS VARCHAR(3)));
+				
+						END
+
+					ELSE
+
+						BEGIN
+
+						SET @VAR_ETABLISSEMENT_CODE_HRS=CONCAT('0',CAST(@VAR_ETABLISSEMENT_CODE_HRS_TEMP AS VARCHAR(3)));
+
+						END
+		
+				Print 'Société en cours '+@VAR_SOCIETE_CEMP+' l établissement '+@VAR_ETABLISSEMENT_CODE_HRU+' devient '+@VAR_ETABLISSEMENT_CODE_HRS-- Affiche du message
+				SET @VAR_COMMENTAIRE='Société en cours '+@VAR_SOCIETE_CEMP+' l établissement '+@VAR_ETABLISSEMENT_CODE_HRU+' devient '+@VAR_ETABLISSEMENT_CODE_HRS;
+				
+				INSERT INTO TRANSCO
+				VALUES (@VAR_TYPE_TRANSCO,@VAR_ETABLISSEMENT_CODE_HRU,@VAR_ETABLISSEMENT_CODE_HRS,@VAR_TRA_SIREN);
+
+				END
+				
+				ELSE --L'établissement existe déjà
+
+				BEGIN
+
+				Print 'L établissement a déjà été traité'
+				SET @VAR_COMMENTAIRE='L établissement a déjà été traité';
+
+				END
+			END
+
+			ELSE--l'établissement ne fait pas partie de la société en cours
+
+			BEGIN
+
+			Print 'L établissement ne fait pas partie de la société'
+			END
+
+
+	SET @VAR_ETABLISSEMENT_LIGNE=@VAR_ETABLISSEMENT_LIGNE+1;
+
+	END
+
+SET @VAR_ETABLISSEMENT_CODE_HRS_TEMP=0;
+SET @VAR_ETABLISSEMENT_LIGNE=1;
+SET @VAR_SOCIETE_LIGNE=@VAR_SOCIETE_LIGNE+1;
+
+END
+
+GO
+
+--Alimentation de la table HR_SPRINT_ETABLISS
+
+DECLARE
+
+@VAR_ET_SIREN varchar(35),
+@VAR_ET_ETABLISSEMENT char(3),
+@VAR_ET_LIBELLE varchar(35),
+@VAR_ET_ABREGE varchar(35),
+@VAR_ET_ADRESSE1 varchar(50),
+@VAR_ET_CODEPOSTAL char(5),
+@VAR_ET_VILLE varchar(35),
+@VAR_ET_DIVTERRIT char(5),
+@VAR_ET_PAYS char(3),
+@VAR_ET_LANGUE char(3),
+@VAR_ET_SOCIETE char(3),
+@VAR_ET_JURIDIQUE char(3),
+@VAR_ET_SIRET char(14),
+@VAR_NIC_ETABLISSEMENT varchar(5),
+@VAR_ET_APE char(5),
+@VAR_CODE_ETAB_HR_SPRINT char(3), --code établissement HR Sprint
+@VAR_CODE_ETAB_HRU char(13),-- code établissement HRU
+@VAR_NBRE_ETABLISSEMENT int,--nbre total d'établissement
+@VAR_ETABLISSEMENT_EN_COURS int; --ligne en cours
+
+SELECT @VAR_ETABLISSEMENT_EN_COURS=MIN(TRA_ID) FROM TRANSCO WHERE TRA_TYPE='Etablissement';
+SELECT @VAR_NBRE_ETABLISSEMENT=MAX(TRA_ID) FROM TRANSCO WHERE TRA_TYPE='Etablissement';
+SET @VAR_ET_DIVTERRIT='';
+SET @VAR_ET_SOCIETE='001';
+SET @VAR_ET_PAYS='FRA';
+SET @VAR_ET_LANGUE='';
+SET @VAR_ET_JURIDIQUE='';
+
+WHILE @VAR_ETABLISSEMENT_EN_COURS<=@VAR_NBRE_ETABLISSEMENT
+
+BEGIN
+
+SELECT @VAR_CODE_ETAB_HR_SPRINT=TRA_VALEURHRS FROM TRANSCO WHERE TRA_ID=@VAR_ETABLISSEMENT_EN_COURS; --récupération du code établissement HRS
+SET @VAR_ET_ETABLISSEMENT=@VAR_CODE_ETAB_HR_SPRINT;
+SELECT @VAR_CODE_ETAB_HRU=TRA_VALEURHRU FROM TRANSCO WHERE TRA_ID=@VAR_ETABLISSEMENT_EN_COURS;--récupération du code établissement HRU
+SELECT @VAR_ET_SIREN=TRA_SIREN FROM TRANSCO WHERE TRA_ID=@VAR_ETABLISSEMENT_EN_COURS; --récupération du SIREN de l'établissement
+
+SELECT TOP(1) @VAR_ET_LIBELLE=[Raison Sociale] FROM ETABLISSEMENT WHERE Etablissement=@VAR_CODE_ETAB_HRU;
+SELECT TOP(1) @VAR_ET_ABREGE=[Raison Sociale] FROM ETABLISSEMENT WHERE Etablissement=@VAR_CODE_ETAB_HRU;
+SELECT TOP(1) @VAR_NIC_ETABLISSEMENT=[Nic] FROM ETABLISSEMENT WHERE Etablissement=@VAR_CODE_ETAB_HRU;--récupération du NIC etablissement
+SET @VAR_ET_SIRET=CONCAT(@VAR_ET_SIREN,@VAR_NIC_ETABLISSEMENT);
+Print @VAR_ET_SIRET
+SELECT TOP(1) @VAR_ET_APE=[Code APET] FROM ETABLISSEMENT WHERE Etablissement=@VAR_CODE_ETAB_HRU;--récupération du NIC etablissement
+SELECT TOP(1) @VAR_ET_ADRESSE1=Voie FROM ETABLISSEMENT WHERE Etablissement=@VAR_CODE_ETAB_HRU;--récupération du NIC etablissement
+SELECT TOP(1) @VAR_ET_CODEPOSTAL=[Code postal] FROM ETABLISSEMENT WHERE Etablissement=@VAR_CODE_ETAB_HRU;--récupération du NIC etablissement
+SELECT TOP(1) @VAR_ET_VILLE=[Localité] FROM ETABLISSEMENT WHERE Etablissement=@VAR_CODE_ETAB_HRU;--récupération du NIC etablissement
+
+INSERT INTO HR_SPRINT_ETABLISS
+VALUES (@VAR_ET_SIREN,@VAR_ET_ETABLISSEMENT,@VAR_ET_LIBELLE,@VAR_ET_ABREGE,@VAR_ET_ADRESSE1,@VAR_ET_CODEPOSTAL,@VAR_ET_VILLE,@VAR_ET_DIVTERRIT,@VAR_ET_PAYS,
+@VAR_ET_LANGUE,@VAR_ET_SOCIETE,@VAR_ET_JURIDIQUE,@VAR_ET_SIRET,@VAR_ET_APE);
+
+PRINT 'Création de l établissement '+@VAR_ET_LIBELLE
+
+SET @VAR_ETABLISSEMENT_EN_COURS=@VAR_ETABLISSEMENT_EN_COURS+1;
+
+END
+
+GO
+
+Print 'Fin reprise Etablissements'
